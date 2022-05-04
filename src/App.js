539a85d1618a52b015ff7bdd1375ee9b0a6f60d7
mateusgomes01 <mgm6@cin.ufpe.br>
2022-05-03 23:20:42 -0300

    Componetizes UserView and RegisterView

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Row from "./components/Row";
-import SearchBox from "./components/SearchBox";
-import Form from "./components/Form";
+import React from "react";
+import UserView from "./components/UserView";
+import RegisterView from "./components/RegisterView";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [render, reRender] = useState(false);
-  const [searchText, setSearchText] = useState("");
-
-  const listUsers = users
-    .filter((user) => user.blocked === false)
-    .filter(
-      (user) =>
-        user.name.toLowerCase().indexOf(searchText.toLowerCase()) !== -1 ||
-        user.username.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
-    )
-    .slice(0, 5)
-    .map((user) => <Row key={user.id} props={user} reRender={reRender} />);
-
-  useEffect(() => {
-    axios
-      .get("https://626bd539e5274e6664d24112.mockapi.io/front-test/v1/user")
-      .then((res) => {
-        console.log(res);
-        setUsers(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, [render]);
-
-  useEffect(() => {
-    console.log(render);
-  }, [render]);
-
   return (
     <>
       <div className="company-container">
@@ -69,76 +39,9 @@ function App() {
       </div>
       <div className="body-container">
         <div className="wrapper">
-          <div className="user-section">
-            <h1>Usuários</h1>
-            <SearchBox
-              searchText={searchText}
-              onSearchTextChange={setSearchText}
-            />
-            <div className="table-wrapper">
-              <table>
-                <thead>
-                  <tr>
-                    <td>USERNAME</td>
-                    <td>NOME</td>
-                    <td>E-MAIL</td>
-                    <td>CIDADE</td>
-                    <td>DIAS DA SEMANA</td>
-                    <td>POSTS</td>
-                    <td>ÁLBUMS</td>
-                  </tr>
-                </thead>
-                <tbody>{listUsers}</tbody>
-              </table>
-            </div>
+          <UserView />
 
-            <div className="table-nav-wrapper">
-              <div>Total 100 show 10 results</div>
-              <div>- Navigation buttons - </div>
-              <div>Jump to page &gt;5</div>
-            </div>
-          </div>
-
-          <div className="register-section">
-            <h1>Registrar</h1>
-            <div className="help-section-wrapper">
-              <div className="help-item">
-                <h4>Precisa de Ajuda?</h4>
-                <div className="help-icon-text">
-                  <div>floater icon</div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                </div>
-              </div>
-              <div className="help-item">
-                <h4>Porque se registrar?</h4>
-                <div className="help-icon-text">
-                  <div>heart icon</div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                </div>
-              </div>
-              <div className="help-item">
-                <h4>O que está rolando</h4>
-                <div className="help-icon-text">
-                  <div>smile logo</div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <Form />
-          </div>
+          <RegisterView />
         </div>
       </div>
 
---
+++ src/components/UserView.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Row from "./Row";
+import SearchBox from "./SearchBox";
+
+function UserView() {
+  const [users, setUsers] = useState([]);
+  const [render, reRender] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  const listUsers = users
+    .filter((user) => user.blocked === false)
+    .filter(
+      (user) =>
+        user.name.toLowerCase().indexOf(searchText.toLowerCase()) !== -1 ||
+        user.username.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+    )
+    .slice(0, 5)
+    .map((user) => <Row key={user.id} props={user} reRender={reRender} />);
+
+  useEffect(() => {
+    axios
+      .get("https://626bd539e5274e6664d24112.mockapi.io/front-test/v1/user")
+      .then((res) => {
+        console.log(res);
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [render]);
+
+  useEffect(() => {
+    console.log(render);
+  }, [render]);
+
+  return (
+    <div className="user-section">
+      <h1>Usuários</h1>
+      <SearchBox searchText={searchText} onSearchTextChange={setSearchText} />
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <td>USERNAME</td>
+              <td>NOME</td>
+              <td>E-MAIL</td>
+              <td>CIDADE</td>
+              <td>DIAS DA SEMANA</td>
+              <td>POSTS</td>
+              <td>ÁLBUMS</td>
+            </tr>
+          </thead>
+          <tbody>{listUsers}</tbody>
+        </table>
+      </div>
+
+      <div className="table-nav-wrapper">
+        <div>Total 100 show 10 results</div>
+        <div>- Navigation buttons - </div>
+        <div>Jump to page &gt;5</div>
+      </div>
+    </div>
+  );
+}
+
+export default UserView;
