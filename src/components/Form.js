3eb5ea04826ae1a299ad2f63489f4bfd68a7a11e
mateusgomes01 <mgm6@cin.ufpe.br>
2022-05-03 23:10:20 -0300

    Form now sends data to api

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Form() {
+  const [formState, setFormState] = useState({
+    username: "",
+    name: "",
+    email: "",
+    city: "",
+  });
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.id]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(
+        "https://626bd539e5274e6664d24112.mockapi.io/front-test/v1/user",
+        formState
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="form-wrapper">
-      <form>
+      <form onSubmit={onSubmit}>
         <h4>Cadastre-se</h4>
         <div className="form-box">
           <div className="input-box">
-            <input type="text" placeholder="Username"></input>
+            <input
+              onChange={(e) => handleChange(e)}
+              id="username"
+              value={formState.username}
+              type="text"
+              placeholder="Username"
+            ></input>
           </div>
           <div className="input-box">
-            <input type="text" placeholder="Nome Completo"></input>
+            <input
+              onChange={(e) => handleChange(e)}
+              id="name"
+              value={formState.name}
+              type="text"
+              placeholder="Nome Completo"
+            ></input>
           </div>
           <div className="input-box">
-            <input type="text" placeholder="E-mail"></input>
+            <input
+              onChange={(e) => handleChange(e)}
+              id="email"
+              value={formState.email}
+              type="text"
+              placeholder="E-mail"
+            ></input>
           </div>
           <div className="input-box">
-            <input type="text" placeholder="Cidade"></input>
+            <input
+              onChange={(e) => handleChange(e)}
+              id="city"
+              value={formState.city}
+              type="text"
+              placeholder="Cidade"
+            ></input>
           </div>
           <div className="radio-wrapper">
             <p>DIAS DA SEMANA</p>
