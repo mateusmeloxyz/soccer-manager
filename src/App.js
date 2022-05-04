531fc3f3437a0b1bb18fbde697d696d633212c84
mateusgomes01 <mgm6@cin.ufpe.br>
2022-05-04 18:35:53 -0300

    Updates icons and header styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from "react";
 import "./App.css";
+import { ReactComponent as SensediaWhiteLogoWithText } from "./img/white-sensedia-logo-text.svg";
+import { ReactComponent as SensediaPurpleLogo } from "./img/purple-sensedia-icon.svg";
+import { ReactComponent as IconArrow } from "./img/icon-arrow.svg";
+import IconHelp from "./img/icon-help.svg";
+import IconWaffleMenu from "./img/icon-waffle-menu.svg";
 import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="company-container">
-        <nav>
-          <div className="logo">Sensedia Soccer Manager</div>
-        </nav>
+        <div>
+          <SensediaWhiteLogoWithText />
+        </div>
       </div>
       <div className="nav-container">
         <nav>
-          <div className="logo">
-            Welcome &gt;
-            <Link to="/users/add">Register</Link> | <Link to="/">Users</Link>
+          <div className="breadcrumbs">
+            <SensediaPurpleLogo />
+            <p>Welcome</p> <IconArrow />
+            <Link className="route-link" to="/users/add">
+              Register
+            </Link>{" "}
+            |{" "}
+            <Link className="route-link" to="/">
+              Users
+            </Link>
           </div>
-          <div className="logo"> help -</div>
-          <div className="logo"> waffle menu</div>
-          <div className="logo"> | User Name</div>
+          <div className="nav-menu">
+            <div className="nav-options">
+              <div className="button-box">
+                <a href="#">
+                  <img src={IconHelp} className="help-logo" alt="help-logo" />
+                </a>
+              </div>
+              <div className="button-box">
+                <a href="#">
+                  <img
+                    src={IconWaffleMenu}
+                    className="help-logo"
+                    alt="help-logo"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="nav-user-options">
+              <div className="user-icon">
+                <p>UN</p>
+              </div>
+              <p>User Name</p>
+            </div>
+          </div>
         </nav>
       </div>
       <div className="header-container">
